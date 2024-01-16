f3fe103d40bd89102776eb66c8e371cc6964010c
Rob <robertpatersonbackup@gmail.com>
2024-01-16 19:34:34 +0000

    Updated

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ add a file called .dotenv or replace your cloudflare api key in main.py.
 
 this script works on linux and windows computers.
 sorry mac users, i do not have a mac to test one with, i suppose you could always fork it.
+
+# the usecase
+this is mainly used for small-scale web servers, like home-based servers.
+this makes setting the A record's ip easier (especially if you use VPNs or iCloud Private Relay like me.)
+
+maybe this is only my scenario, but oh well it's here now.
--- main.py
+++ main.py
@@ -3,9 +3,20 @@ import os
 import sys
 from dotenv import load_dotenv
 
+def defineOS():
+    platform = sys.platform
+    if platform == "win32":
+        return "windows"
+    elif platform == "linux" or sys.platform == "linux2":
+        return "linux"
+def networkCommands():
+    windows_commands=["PowerShell Invoke-RestMethod ipinfo.io/ip"]
+    linux_commands=["dig +short myip.opendns.com @resolver1.opendns.com"]
+
 load_dotenv()
 
 # You can replace this with your actual CloudFlare API key,
 # I just use dotenv to be a bit more safe.
 cloudFlareSuperSecretApiKey = os.getenv('cloudFlareSuperSecretApiKey')
+
 
